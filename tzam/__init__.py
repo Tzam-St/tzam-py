4523a44448e3872f3266acfe8e317ab0fc3c82a8
Carlos Carvalho <contato@carlosocarvalho.com.br>
2026-04-23 13:20:41 -0300

    chore: bump version to 0.3.0

--- tzam/__init__.py
+++ tzam/__init__.py
@@ -35,7 +35,7 @@ from ._errors import (
 )
 from ._types import AppConfig, AppMethods, Config, LoginResult, OAuthMethods, TokenPayload, User
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     # client
